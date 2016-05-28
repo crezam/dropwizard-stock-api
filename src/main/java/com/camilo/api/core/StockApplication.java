c95e5192c5c4e5600726623f67860fbbdd881ee1
Camilo Crespo <crezam@live.com>
2016-05-27 23:44:55 -0400

    Overriding as needed

--- src/main/java/com/camilo/api/core/StockApplication.java
+++ src/main/java/com/camilo/api/core/StockApplication.java
@@ -1,4 +1,28 @@
 package com.camilo.api.core;
 
-public class StockApplication {
+import com.camilo.api.configuration.StockApiConfiguration;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class StockApplication extends Application<StockApiConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new StockApplication().run(args);
+    }
+
+    @Override
+    public String geName() {
+        return "stock-api";
+    }
+
+    @Override
+    public void initialize(Bootstrap<StockApiConfiguration> bootstrap) {
+
+    }
+
+    @Override
+    public void run(StockApiConfiguration stockApiConfiguration, Environment environment) throws Exception {
+
+    }
 }
