7e8bc8c65549cad71b9aedac20b43a1e9f466533
Camilo Crespo <crezam@live.com>
2016-05-27 23:31:39 -0400

    Config fields

--- src/main/java/com/camilo/api/configuration/StockApiConfiguration.java
+++ src/main/java/com/camilo/api/configuration/StockApiConfiguration.java
@@ -1,4 +1,22 @@
 package com.camilo.api.configuration;
 
-public class StockApiConfiguration {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class StockApiConfiguration extends Configuration {
+
+    @NotEmpty
+    private String defaultStock = "CCZ";
+
+    @JsonProperty
+    public String getDefaultStock() {
+        return defaultStock;
+    }
+
+    @JsonProperty
+    public void setDefaultStock(String stock) {
+        this.defaultStock = stock;
+    }
+
 }
