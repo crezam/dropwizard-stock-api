40a1ff486d789a05a5bee07bbe3a3634d2d2c660
Camilo Crespo <crezam@live.com>
2016-06-04 23:32:22 -0400

    Adding to model

--- src/main/java/com/camilo/api/model/StockQuote.java
+++ src/main/java/com/camilo/api/model/StockQuote.java
@@ -1,4 +1,32 @@
 package com.camilo.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+
 public class StockQuote {
+
+    private long id;
+
+    @Length(max = 3)
+    private String price;
+
+    public StockQuote() {
+        // Jackson deserialization
+    }
+
+    public StockQuote(long id, String price) {
+        this.id = id;
+        this.price = price;
+    }
+
+    @JsonProperty
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public String getPrice() {
+        return price;
+    }
+
 }
