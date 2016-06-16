d0c99af8a0e6ac80b0fbb08f4f6ab1eb1f908449
Camilo Crespo <crezam@live.com>
2016-06-16 00:12:15 -0400

    Imp health check

--- src/main/java/com/camilo/api/health/StockHealthCheck.java
+++ src/main/java/com/camilo/api/health/StockHealthCheck.java
@@ -1,4 +1,27 @@
 package com.camilo.api.health;
 
-public class StockHealthCheck {
+import com.codahale.metrics.health.HealthCheck;
+
+public class StockHealthCheck extends HealthCheck {
+
+    private final String stock;
+
+    public StockHealthCheck(String stock) {
+        super();
+        this.stock = stock;
+    }
+
+    @Override
+    protected Result check() throws Exception {
+        final String saying = String.format(stock, "TEST");
+        if(!saying.contains("TEST")) {
+            return Result.unhealthy("Bad response");
+        }
+        return Result.healthy();
+    }
+
+    @Override
+    public Result execute() {
+        return super.execute();
+    }
 }
